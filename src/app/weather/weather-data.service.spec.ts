7c664d214bf9ff571a4c65e2fcc702f73c05648b
pendraic <caboyof@gmail.com>
2021-10-25 17:27:49 +0800

    Created tests for services with dependencies

--- src/app/weather/weather-data.service.spec.ts
+++ src/app/weather/weather-data.service.spec.ts
@@ -1,18 +1,41 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import { appStateReducer } from '../app-state.reducers';
 import { WeatherDataService } from './weather-data.service';
 
 describe('WeatherDataService', () => {
   let service: WeatherDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        StoreModule.forRoot({ appState: appStateReducer }),
+      ],
+    });
     service = TestBed.inject(WeatherDataService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-    // TODO
-    pending('WeatherDataService');
+  });
+
+  describe('localFileWeather()', () => {
+    it('should return an Observable when called', () => {
+      const result = service.localFileWeather();
+
+      expect(result).toBeInstanceOf(Observable);
+    });
+  });
+
+  describe('localFileGeolocation()', () => {
+    it('should return an Observable when called', () => {
+      const result = service.localFileGeolocation();
+
+      expect(result).toBeInstanceOf(Observable);
+    });
   });
 });
--- src/app/weather/weather-mode.service.spec.ts
+++ src/app/weather/weather-mode.service.spec.ts
@@ -1,18 +1,104 @@
 import { TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 
+import { appStateReducer } from '../app-state.reducers';
 import { WeatherModeService } from './weather-mode.service';
 
 describe('WeatherModeService', () => {
   let service: WeatherModeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({ appState: appStateReducer })],
+    });
     service = TestBed.inject(WeatherModeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-    // TODO
-    pending('WeatherModeService');
+  });
+
+  describe('isCurrent()', () => {
+    it('should return true if set to the expected value', () => {
+      service.changeToCurrent();
+
+      expect(service.isCurrent).toBeTruthy();
+    });
+
+    it('should return true if set to the expected value from another value', () => {
+      service.changeToDaily();
+      service.changeToCurrent();
+
+      expect(service.isCurrent).toBeTruthy();
+    });
+
+    it('should return false if set to another value', () => {
+      service.changeToDaily();
+
+      expect(service.isCurrent).toBeFalsy();
+    });
+
+    it('should return false if set to another value from expected value', () => {
+      service.changeToCurrent();
+      service.changeToHourly();
+
+      expect(service.isCurrent).toBeFalsy();
+    });
+  });
+
+  describe('isHourly()', () => {
+    it('should return true if set to the expected value', () => {
+      service.changeToHourly();
+
+      expect(service.isHourly).toBeTruthy();
+    });
+
+    it('should return true if set to the expected value from another value', () => {
+      service.changeToDaily();
+      service.changeToHourly();
+
+      expect(service.isHourly).toBeTruthy();
+    });
+
+    it('should return false if set to another value', () => {
+      service.changeToDaily();
+
+      expect(service.isHourly).toBeFalsy();
+    });
+
+    it('should return false if set to another value from expected value', () => {
+      service.changeToHourly();
+      service.changeToCurrent();
+
+      expect(service.isHourly).toBeFalsy();
+    });
+  });
+
+  describe('isDaily()', () => {
+    it('should return true if set to the expected value', () => {
+      service.changeToDaily();
+
+      expect(service.isDaily).toBeTruthy();
+    });
+
+    it('should return true if set to the expected value from another value', () => {
+      service.changeToCurrent();
+      service.changeToDaily();
+
+      expect(service.isDaily).toBeTruthy();
+    });
+
+    it('should return false if set to another value', () => {
+      service.changeToCurrent();
+
+      expect(service.isDaily).toBeFalsy();
+    });
+
+    it('should return false if set to another value from expected value', () => {
+      service.changeToDaily();
+      service.changeToHourly();
+
+      expect(service.isDaily).toBeFalsy();
+    });
   });
 });
