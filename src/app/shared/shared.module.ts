df6c4b3c6828d7715d8c13a147f88d6026f2eb79
pendraic <caboyof@gmail.com>
2021-11-01 20:04:11 +0800

    Prep for searchbar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,16 +9,22 @@ import { MatDialogModule } from '@angular/material/dialog';
 
 import { MenuToolbarComponent } from './menu-toolbar/menu-toolbar.component';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
-  declarations: [MenuToolbarComponent, ErrorDialogComponent],
+  declarations: [
+    MenuToolbarComponent,
+    ErrorDialogComponent,
+    SearchbarComponent,
+  ],
   imports: [
     CommonModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
     MatIconModule,
     MatToolbarModule,
   ],
-  exports: [MenuToolbarComponent, ErrorDialogComponent],
+  exports: [MenuToolbarComponent, ErrorDialogComponent, SearchbarComponent],
 })
 export class SharedModule {}
