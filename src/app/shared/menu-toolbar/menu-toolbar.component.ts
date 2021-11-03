d7e57e0553154fac5cf842384397b325166bb34c
pendraic <caboyof@gmail.com>
2021-11-03 14:21:42 +0800

    Side navigation menu and toolbar

--- src/app/shared/menu-toolbar/menu-toolbar.component.ts
+++ src/app/shared/menu-toolbar/menu-toolbar.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'shared-menu-toolbar',
   templateUrl: './menu-toolbar.component.html',
-  styleUrls: ['./menu-toolbar.component.css'],
+  styleUrls: ['./menu-toolbar.component.scss'],
 })
 export class MenuToolbarComponent {
   @Input() title!: string;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { MenuToolbarComponent } from './menu-toolbar/menu-toolbar.component';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
@@ -24,6 +26,8 @@ import { SearchbarComponent } from './searchbar/searchbar.component';
     MatFormFieldModule,
     MatIconModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   exports: [MenuToolbarComponent, ErrorDialogComponent, SearchbarComponent],
 })
