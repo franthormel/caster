e91841a584979d6e0bb53df2ef99462621328a96
pendraic <caboyof@gmail.com>
2021-11-09 14:37:50 +0800

    Air pollution tooltips

--- src/app/modules/air-pollution/air-pollution.module.ts
+++ src/app/modules/air-pollution/air-pollution.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AirPollutionRoutingModule } from './air-pollution-routing.module';
 import { AirPollutionComponent } from './air-pollution/air-pollution.component';
@@ -19,6 +20,7 @@ import { SharedModule } from '../shared/shared.module';
     MatButtonModule,
     MatIconModule,
     MatProgressBarModule,
+    MatTooltipModule,
   ],
   providers: [AirQualityService],
 })
