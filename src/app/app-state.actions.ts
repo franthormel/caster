1390f568a152e4e767a7d8eec97dd15ebbf995df
pendraic <caboyof@gmail.com>
2021-11-17 07:38:34 +0800

    Deleted fil

--- src/app/app-state.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-// TODO Move to locaitons module
-export const changeStaticFile = createAction(
-  '[Static Data] File Update',
-  props<{ file: number }>()
-);
