0926ecc6e3199f6d1000f01ea43d44b46d887f8d
neretzes <caboyof@gmail.com>
2021-12-02 13:57:11 +0800

    Updated animations from 250 to 300ms

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,5 +1,5 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 
 export const fadeInElement = trigger('fadeInElement', [
-  transition('* => *', [style({ opacity: 0 }), animate('250ms ease-in-out')]),
+  transition('* => *', [style({ opacity: 0 }), animate('300ms ease-in-out')]),
 ]);
